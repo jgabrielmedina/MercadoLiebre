5803a8450171940a8e27bfecad19471881ed9090
jgabrielmedina <jogabrielmedina@gmail.com>
2023-02-22 19:41:11 -0300

    proyecto adaptado para render

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const path = require('path')
 
 app.use(express.static('public'))
 
-app.listen(3000, ()=>{
-    console.log("servidor corriendo")
+
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, ()=>{
+    console.log("servidor corriendo en "+ port)
 })
 
 app.get('/',function(req,res){
     res.sendFile(path.resolve(__dirname,'./views/home.html'))
 })
-
-/* app.get('/home',function(req,res){
-    res.sendFile(path.resolve(__dirname,'./views/home2.html'))
-}) */
 
 app.get('/register',function(req,res){
     res.sendFile(path.resolve(__dirname,'./views/register.html'))
